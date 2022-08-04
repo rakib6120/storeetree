c1ae8e884077618b7e2670d91209f08dba4c7d9b
Rakib Hossain <rakibhossain6120@gmail.com>
2022-08-04 23:31:16 +0600

    Added text botton center of video.

--- app/Http/Controllers/frontend/VideoRecordingController.php
+++ app/Http/Controllers/frontend/VideoRecordingController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\frontend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Question;
 use Illuminate\Support\Facades\Auth;
-use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
+use FFMpeg\Filters\Video\VideoFilters;
 use Illuminate\Support\Facades\Session;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class VideoRecordingController extends Controller
 {
+    protected function word_chunk($str, $len = 76, $end = "\n")
+    {
+        $pattern = '~.{1,' . $len . '}~u'; // like "~.{1,76}~u"
+        $str = preg_replace($pattern, '$0' . $end, $str);
+        return rtrim($str, $end);
+    }
+
     public function store(Request $request)
     {
         $request->validate([
@@ -17,10 +26,17 @@ class VideoRecordingController extends Controller
             'question_id' => "required"
         ]);
 
+        $question   = Question::findOrFail($request->question_id);
         $storyItems = (array) Session::get('storyItems');
         $video_storage_link = "chunk-video/" . Auth::user()->id . "/" . date('Y-m-d') . "/" . rand(99999, 9999999) . time() . ".mp4";
 
+        $text = $this->word_chunk($question->title);
+        $command = "text='$text': fontcolor=black: fontsize=16: box=1: boxcolor=white@0.4: boxborderw=6: x=(w-text_w)/2:y=h-th-1:";
+
         FFMpeg::open($request->file('video'))
+            ->addFilter(function (VideoFilters $filters) use ($command) {
+                return $filters->custom("drawtext=$command");
+            })
             ->export()
             ->toDisk('public')
             ->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))
@@ -34,7 +50,7 @@ class VideoRecordingController extends Controller
                 $isNewItem = false;
                 break;
             }
-        }        
+        }
 
         if ($isNewItem) {
             $storyItems[] = [
