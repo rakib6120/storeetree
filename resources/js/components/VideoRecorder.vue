1b2fbf02129b2cce9d40ed7d542e31ffd0a17251
Rakib Hossain <rakibhossain6120@gmail.com>
2022-08-10 00:17:23 +0600

    remove reload.

--- resources/js/components/VideoRecorder.vue
+++ resources/js/components/VideoRecorder.vue
@@ -185,9 +185,10 @@ export default {
                 }
             }).then(
                 res => {
+                    console.log(res);
                     if (res.status === 200) {
                         this.submitText = "Upload Complete";
-                        window.location.reload();
+                        // window.location.reload();
                     } else {
                         Swal.fire({
                             icon: 'error',
@@ -199,6 +200,7 @@ export default {
                 }
             ).catch(
                 error =>{
+                    console.log(error);
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
